d9dddf653ffd357e2ac799b6784c48ba83ddb236
witkowski.mar@gmail.com <witkowski.mar@gmail.com>
2022-03-07 17:02:16 +0100

    fix debug

--- src/main/java/debug/DebugExample.java
+++ src/main/java/debug/DebugExample.java
@@ -10,10 +10,10 @@ public class DebugExample {
 
         int silnia = 1;
 
-        for(int i = 0;i<15;i++ ) {
+        for(int i = 0;i<=19;i++ ) {
             silnia = i*silnia;
         }
 
-        log.info("Silnia 15 : " + silnia);
+        log.info("Silnia 19 : " + silnia);
     }
 }
